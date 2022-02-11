01e282f0b85813acc63e3320b44a7655810685f7
EderZarante <99446715+EderZarante@users.noreply.github.com>
2022-02-11 10:22:22 -0500

    a little change

--- src/public/sockets.js
+++ src/public/sockets.js
@@ -1,10 +1,6 @@
 const socket = io.connect();
 
-/**
- * create a new note
- * @param {string} title a title for a new note
- * @param {string} description a description for a new note
- */
+
 const saveArticle = (title, description) => {
   socket.emit("client:newarticle", {
     title,
@@ -12,20 +8,11 @@ const saveArticle = (title, description) => {
   });
 };
 
-/**
- * delete a note based on an Id
- * @param {string} id a note ID
- */
+
 const deleteArticle = (id) => {
   socket.emit("client:deletearticle", id);
 };
 
-/**
- * 
- * @param {string} id note ID
- * @param {string} title note title 
- * @param {string} description note description
- */
 const updateArticle= (id, title, description) => {
   socket.emit("client:updatearticle", {
     id,
